eb58cfcbcae1f95a8f56d7945e5bc7ccc5ba6f0b
abantej <joeward.l.abante@gmail.com>
2020-03-05 20:43:35 +0800

    initial commit

---
+++ src/components/Workout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h3>Workout Checklist:</h3>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Workout',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
