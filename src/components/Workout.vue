1fcec1af5940d1893c7cdf3b3cf86e91d1ef8b30
abantej <joeward.l.abante@gmail.com>
2020-03-07 18:12:52 +0800

    added edit function

--- src/components/Workout.vue
+++ src/components/Workout.vue
@@ -3,8 +3,19 @@
     <input type="text" class="workout-input" placeholder="What needs to be done"
            v-model="newWorkout" @keyup.enter="addWorkout">
     <div v-for="(workout, index) in workouts" :key="workout.id" class="workout-item">
-      <div>
-        {{ workout.title }}
+      <div class="workout-item-left">
+        <div v-if="!workout.editing"
+             @dblclick="editWorkout(index)"
+             class="workout-item-label">
+          {{ workout.title }}
+        </div>
+        <input v-else class="workout-item-edit"
+               type="text"
+               v-model="workout.title"
+               @blur="doneEdit(index)"
+               @keyup.enter="doneEdit(index)"
+               @keyup.esc="cancelEdit(index)"
+               v-focus>
       </div>
       <div class="remove-item" @click="removeWorkout(index)">
         &times;
@@ -24,20 +35,30 @@ export default Vue.extend({
   data() {
     return {
       newWorkout: '',
+      beforeEditCache: '',
       idForWorkout: 3,
       workouts: [
         {
           id: 1,
           title: 'Pull ups',
           completed: false,
+          editing: false,
         },
         {
           id: 2,
           title: 'Push ups',
           completed: false,
+          editing: false,
         },
       ],
     };
+  },
+  directives: {
+    focus: {
+      inserted(el) {
+        el.focus();
+      },
+    },
   },
   methods: {
     addWorkout() {
@@ -52,8 +73,19 @@ export default Vue.extend({
       this.newWorkout = '';
       this.idForWorkout += 1;
     },
+    editWorkout(index) {
+      this.beforeEditCache = this.workouts[index].title;
+      this.workouts[index].editing = true;
+    },
+    doneEdit(index) {
+      this.workouts[index].editing = false;
+    },
     removeWorkout(index) {
       this.workouts.splice(index, 1);
+    },
+    cancelEdit(index) {
+      this.workouts[index].title = this.beforeEditCache;
+      this.workouts[index].editing = false;
     },
   },
 });
@@ -83,6 +115,31 @@ export default Vue.extend({
     margin-left: 14px;
     &:hover {
       color: black;
+    }
+  }
+
+  .workout-item-left {
+    display: flex;
+    align-items: center;
+  }
+
+  .workout-item-label {
+    padding: 18px;
+    border: 1px solid white;
+    margin-left: 12px;
+  }
+
+  .workout-item-edit {
+    font-size: 24px;
+    color: #2c3e50;
+    margin-left: 12px;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+
+    &:focus {
+      outline: none;
     }
   }
 </style>
